109f0a8a51c098be91517a4af03a905869293d5d
whddnjs1715 <whddnjs1715@naver.com>
2021-09-02 09:20:42 +0900

    폴더구조 세팅 #1

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, } from 'react-native';
+import Index from './src/index'
+import Utils from './src/utils'
+import { Provider } from 'react-redux'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={Utils}>
+        <Index />
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e9ecef',
   },
 });
