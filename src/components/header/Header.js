f2838ca95ba4999a33c5e3b5b0d2cf283c857cf5
whddnjs1715 <whddnjs1715@naver.com>
2021-09-02 09:34:14 +0900

    create header #2

---
+++ src/components/header/Header.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image, } from 'react-native'
+import Logo from '../../../assets/icon.png';
+
+const Header = () => {
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    });
+    const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
+    
+    return (
+        <View style={styles.headerLayout}>
+            <View style={styles.headerLayer}>
+                <Image 
+                    style={styles.headerImg}
+                    source={Logo}
+                />
+                <View style={styles.headerContainer}>
+                    <Text>{dateString}</Text>
+                    <Text style={{textAlign: 'center'}}>{dayName}</Text>
+                </View>
+            </View>
+            <Text style={styles.headerTitle}>
+                Todo List
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerLayout: {
+        marginTop: 30,
+        marginBottom : 30,
+        marginLeft: 16,
+    },
+    headerLayer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    headerImg: {
+        resizeMode:'contain',
+        height: '60%',
+        width:'20%',
+        justifyContent: 'flex-end',
+    },
+    headerContainer: {
+        marginRight: 10,
+    },
+    headerTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        fontSize: 30,
+    }
+})
+
+export default Header 
